45fe82289fbf7faf8a2ceb398a1be288de572826
Ayomide <ayomidedans@gmail.com>
2022-10-31 22:36:45 +0100

    feat: landing page redesign

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import { AiOutlineTwitter, AiOutlineMail } from 'react-icons/ai';
 import { FaFacebookF } from 'react-icons/fa';
 import { Head } from '../assets';
@@ -7,29 +8,51 @@ import { FACEBOOK_URL, TWITTER_URL, EMAIL_URL } from '../constants';
 
 const Header = () => {
   return (
-    <div id='home' className="h-full flex flex-col lg:flex-row md:justify-between lg:mt-20 md:mt-10 mt-6 px-4 md:px-8 lg:px-10 md:mb-10">
-      <div className="flex flex-col">
-        <h3 className='uppercase font-dmsans font-semibold text-text-orange lg:text-lg md:text-base text-sm text-[#FFA217]'>
+    <motion.div
+      id='home' className="h-full flex flex-col lg:flex-row md:justify-between lg:mt-20 md:mt-10 mt-6 px-4 md:px-8 lg:px-10 md:mb-10">
+      <div
+        className="flex flex-col">
+        <motion.h3
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, ease: [0.6, 0.01, -0.05, 0.9] }}
+          className='uppercase font-dmsans font-semibold text-text-orange lg:text-lg md:text-base text-sm text-[#FFA217]'>
           Cargo and Logistics
-        </h3>
-        <h1 className='capitalize font-manrope font-extrabold text-black lg:text-[3rem] md:text-[3rem] text-[1.8rem] lg:pr-1'>
+        </motion.h3>
+        <motion.h1
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.1, ease: [0.6, 0.01, -0.05, 0.9] }}
+          className='capitalize font-manrope font-extrabold text-black lg:text-[3rem] md:text-[3rem] text-[1.8rem] lg:pr-1'>
           Making shippins fast and safe
-        </h1>
-        <h2 className="font-manrope sm:leading-5 text-text-black md:text-sm lg:pr-10 sm:text-xs text-[0.6rem] leading-3 tracking-[0.01rem]">
+        </motion.h1>
+        <motion.h2
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.3, ease: [0.6, 0.01, -0.05, 0.9] }}
+          className="font-manrope sm:leading-5 text-text-black md:text-sm lg:pr-10 sm:text-xs text-[0.6rem] leading-3 tracking-[0.01rem]">
           We are a community of bold minds who have decided to work under the same roof. By saying that our SmartSpace is sustainable and comfy we confirm that you will get 100% satisfaction from working here and making the best out of this experience
-        </h2>
+        </motion.h2>
 
-        <div className="flex flex-row mt-6 md:mt-10">
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.4, ease: [0.6, 0.01, -0.05, 0.9] }}
+          className="flex flex-row mt-6 md:mt-10">
           <FaFacebookF className='md:w-9 w-7 md:h-9 h-7 shadow p-2 cursor-pointer' onClick={() => { window.open(FACEBOOK_URL, '_blank'); }} />
           <AiOutlineTwitter className='md:w-9 w-7 md:h-9 h-7 ml-5 shadow p-1 cursor-pointer' onClick={() => { window.open(TWITTER_URL, '_blank'); }} />
           <AiOutlineMail className='md:w-9 w-7 md:h-9 h-7 ml-5 shadow p-1 cursor-pointer' onClick={() => { window.open(EMAIL_URL, '_blank'); }} />
-        </div>
+        </motion.div>
       </div>
 
-      <div className="flex lg:mr-4 lg:-mt-10 md:-mt-6 mt-0 justify-center">
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.5, ease: [0.6, 0.01, -0.05, 0.9] }}
+        className="flex lg:mr-4 lg:-mt-10 md:-mt-6 mt-0 justify-center">
         <img src={Head} alt="House" className='lg:w-[70rem] lg:h-96 md:w-[50rem] w-full md:h-[30rem] lg:-ml-0 mt-4 md:mt-10' />
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
--- src/containers/Testimonial.jsx
+++ src/containers/Testimonial.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import { testimony } from '../constants';
 
 import { Testimonies } from '../components';
 
 const Testimonial = () => {
   return (
-    <div className='flex flex-col my-10 md:mt-20 md:mx-10 mx-3'>
+    <div className='flex flex-col my-10 md:mt-20 md:mx-10 mx-5'>
       <div className="flex md:mr-40">
-        <h1 className="font-manrope text-sm md:text-3xl lg:w-1/2 font-semibold md:pb-3 pb-1">
+        <h1 className="font-manrope text-xl md:text-3xl w-5/6 lg:w-1/2 font-semibold md:pb-3 pb-1">
           Loved by businesses, and individuals across the globe.
         </h1>
       </div>
 
-      <div className="md:grid md:grid-cols-3 lg:gap-10 mb-10 mt-4 md:gap-5 flex flex-col">
+      <div className="md:grid md:grid-cols-3 lg:gap-10 mb-10 mt-4 md:gap-5 flex flex-col px-5 md:px-0">
         {testimony.map((item, index) => (
           <Testimonies key={item.name + index} review={item.review}
             text={item.text} dp={item.dp} name={item.name} city={item.city}
